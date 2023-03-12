84e76b3627bf82425ce38ad3e4a799b3d984ec3b
SemihKokcu <semihkkc1@gmail.com>
2023-03-12 21:49:52 +0300

    login added

--- ECommerenceAPI.API/Controllers/UsersController.cs
+++ ECommerenceAPI.API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerenceAPI.Application.Features.Commands.AppUser.CreateUser;
+using ECommerenceAPI.Application.Features.Commands.AppUser.LoginUser;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +23,14 @@ namespace ECommerenceAPI.API.Controllers
             CreateUserCommandResponse createUserCommandResponse = await _mediator.Send(createUserCommandRequest);
 
             return Ok(createUserCommandResponse);
+        }
+
+        [HttpPost("action")]
+        public async Task<IActionResult> Login(LoginUserCommandRequest loginUserCommandRequest)
+        {
+            LoginUserCommandResponse loginUserCommandResponse = await _mediator.Send(loginUserCommandRequest);
+
+            return Ok(loginUserCommandResponse);
         }
     }
 }
